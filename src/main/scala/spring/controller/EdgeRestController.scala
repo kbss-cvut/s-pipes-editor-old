3a8171e79ef595ea039c183042f88e6d79c7554b
Yan Doroshenko <yan1994doroshenko@gmail.com>
2016-10-30 22:19:04 +0100

    Some JSON parsin'

--- src/main/scala/spring/controller/EdgeRestController.scala
+++ src/main/scala/spring/controller/EdgeRestController.scala
@@ -2,11 +2,12 @@ package spring.controller
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.{HttpStatus, ResponseEntity}
-import org.springframework.web.bind.annotation.{DeleteMapping, PostMapping, RequestBody, RestController}
+import org.springframework.web.bind.annotation._
 import spring.model.dao.NodeDao
 import spring.model.graph.Edge
 
 import scala.beans.BeanProperty
+import scala.util.parsing.json.JSON
 
 /**
   * Created by Yan Doroshenko (yandoroshenko@protonmail.com) on 28.10.16.
@@ -27,7 +28,19 @@ class EdgeRestController {
   }
 
   @DeleteMapping(Array("/"))
-  def deleteEdge(@RequestBody ids: (Long,Long)) = {
-    new ResponseEntity("Edge deleted", HttpStatus.OK)
+  def deleteEdge(@RequestBody json: String) = {
+    JSON parseFull json match {
+      case Some(parsed) =>
+        val map = parsed.asInstanceOf[Map[String, Double]]
+        val from = map("from") toLong
+        val to = map("to") toLong
+
+        println(from)
+        println(to)
+
+        new ResponseEntity("Edge deleted", HttpStatus.OK)
+      case None =>
+        new ResponseEntity("Request syntax error", HttpStatus.BAD_REQUEST)
+    }
   }
 }
