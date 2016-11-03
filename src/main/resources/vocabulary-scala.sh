d42df64a9792ba290165488acb7145cef0a240ae
Yan Doroshenko <yan1994doroshenko@gmail.com>
2016-11-03 18:00:06 +0100

    Generating Scala vocabulary instead of Java

---
+++ src/main/resources/vocabulary-scala.sh
@@ -0,0 +1,11 @@
+java_vocabulary=$(find ~/Dropbox/studies/EAR/SPEd -path "*/model/Vocabulary.java")
+scala_vocabulary="$(echo $java_vocabulary | rev | cut -d'/' -f2- |rev)/Vocabulary.scala"
+echo > $scala_vocabulary
+grep package $java_vocabulary >> $scala_vocabulary
+echo >> $scala_vocabulary
+echo "object Vocabulary {" >> $scala_vocabulary
+echo >> $scala_vocabulary
+grep "String" $java_vocabulary | cut -d' ' -f 9,10,11 | awk '{print "final val "$0}' >> $scala_vocabulary
+echo >> $scala_vocabulary
+echo "}" >> $scala_vocabulary
+rm $java_vocabulary
