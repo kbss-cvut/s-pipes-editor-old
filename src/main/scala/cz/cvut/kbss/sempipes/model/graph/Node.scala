cdd832e2006937edbad5792e309e1916fabf670b
Miroslav <blcham@gmail.com>
2016-11-03 12:25:30 +0100

    [new] annotations for ontological model

--- src/main/scala/cz/cvut/kbss/sempipes/model/graph/Node.scala
+++ src/main/scala/cz/cvut/kbss/sempipes/model/graph/Node.scala
@@ -2,28 +2,35 @@ package spring.model.graph
 
 import java.net.URI
 
-import cz.cvut.kbss.jopa.model.annotations.Id
+import cz.cvut.kbss.jopa.model.annotations.{Id, OWLClass, OWLDataProperty, Types}
+import cz.cvut.kbss.sempipes.model.Vocabulary
 
 import scala.beans.BeanProperty
 
 /**
   * Created by Yan Doroshenko (yandoroshenko@protonmail.com) on 28.10.16.
   */
-class Node(@Id(generated = true)
-            @BeanProperty
+@OWLClass(iri = Vocabulary.s_c_node)
+class Node(@BeanProperty
+           @Id(generated = true)
            uri: URI,
            @BeanProperty
+           @OWLDataProperty(iri = Vocabulary.s_p_has_x_coordinate)
            x: Double,
            @BeanProperty
+           @OWLDataProperty(iri = Vocabulary.s_p_has_y_coordinate)
            y: Double,
            @BeanProperty
-           nodeType: Any, // I have no idea what I'm doing
+           @Types
+           nodeType: Set[String],
            @BeanProperty
-           incoming: Option[Seq[String]],
+           @OWLDataProperty(iri = Vocabulary.s_p_has_input_parameter)
+           inParameters: Set[String],
            @BeanProperty
-           outgoing: Option[Seq[String]]) {}
+           @OWLDataProperty(iri = Vocabulary.s_p_has_output_parameter)
+           outParameters: Set[String]) {}
 
 object Node {
-  def apply(uri: URI, x: Double, y: Double, nodeType: Any, incoming: Option[Seq[String]], outgoing: Option[Seq[String]]): Node =
-    new Node(uri, x, y, nodeType, incoming, outgoing)
+  def apply(uri: URI, x: Double, y: Double, nodeType: Set[String], inParameters: Set[String], outParameters: Set[String]): Node =
+    new Node(uri, x, y, nodeType, inParameters, outParameters)
 }
