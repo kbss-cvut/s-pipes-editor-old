042d0514d3963ca62092ef17072a9c8435aa81de
YanDoroshenko <yan1994doroshenko@gmail.com>
2016-11-17 14:14:27 +0100

    Attempt to use JSON-LD deserializer. FAILED

---
+++ src/main/scala/cz/cvut/kbss/sempipes/config/RestConfig.scala
@@ -0,0 +1,25 @@
+package cz.cvut.kbss.sempipes.config
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.databind.{DeserializationFeature, ObjectMapper}
+import cz.cvut.kbss.jsonld.jackson.JsonLdModule
+import org.springframework.context.annotation.{Bean, ComponentScan, Configuration}
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+
+/**
+  * Created by Yan Doroshenko (yandoroshenko@protonmail.com) on 16.11.16.
+  */
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = Array("cz.cvut.kbss.sempipes"))
+class RestConfig {
+  @Bean
+  def objectMapper: ObjectMapper = {
+    val objectMapper: ObjectMapper = new ObjectMapper
+    objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL)
+    objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+    // Here we register the JSON-LD serialization/deserialization module
+    objectMapper.registerModule(new JsonLdModule)
+    objectMapper
+  }
+}
