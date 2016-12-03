b06569a0a1a2742be7af35c09863e2f649286660
Artem Kelpe <kelpeart@fel.cvut.cz>
2016-12-03 05:03:21 +0100

    Progress with drawing nodes. Some edits for the SRS (checkpoint1).

--- experiments_with_JS/backup/graph.js
+++ experiments_with_JS/backup/graph.js
@@ -1,10 +1,11 @@
 
 
 sigma.utils.pkg('sigma.canvas.nodes');
+sigma.utils.pkg('sigma.canvas.edges');
 sigma.utils.pkg('sigma.canvas.labels');
 
 //////////////////////////////////////////
-/////  creating new shape renderer
+/////  creating new node renderer
 ///////////////////////////////////////////
 sigma.canvas.nodes.def = (function() {
   var renderer = function(node, context, settings) {
@@ -13,9 +14,10 @@ sigma.canvas.nodes.def = (function() {
         textWidth,
         inParams = node.inParams,
         outParams = node.outParams;
+        inParamsZones = node.inParamsZones;
+        outParamsZones = node.outParamsZones;
     size = node[prefix + 'size'];
     textWidth = context.measureText(node.label).width;
-
 
     textWidthLabel = context.measureText(node.label).width;
 
@@ -38,17 +40,6 @@ sigma.canvas.nodes.def = (function() {
     else if (textWidthLabel < allInsLength*6 && textWidthLabel < allOutsLength*6 && allOutsLength < allInsLength)
       textWidth = allInsLength*6;
 
-    console.log(
-    "textWidthLabel=",
-    textWidthLabel,
-    " allInsLength=",
-    allInsLength,
-    " allOutsLength=",
-    allOutsLength,
-    " textWidth=",
-    textWidth
-  );
-
     var nodeXX = (node[prefix + 'x'] - (textWidth * 1.2) * 0.5) - 50,
         nodeYY = node[prefix + 'y'] - size * 1.2 * 0.8,
         nodeWidth = textWidth * 1.2 + 50 * 2,
@@ -68,23 +59,36 @@ sigma.canvas.nodes.def = (function() {
         nodeWidth,
         nodeHeight
       ); 
+    node.coordinates[0] = nodeXX;
+    node.coordinates[1] = nodeYY;
+    node.coordinates[2] = nodeXX + nodeWidth;
+    node.coordinates[3] = nodeYY + nodeHeight;
     context.closePath();
     context.fill();
     context.stroke();
 
+    var rectX, 
+        rectY, 
+        rectWidth, 
+        rectHeight;
+
     //draw upper rect(s)
-    
     var temp = 0;
     for (i = 0; i < inParams.length; i++){
         temp = 0;
         context.fillStyle = node.color;
         context.beginPath();
+
         if (i == 0) {
+          rectX = nodeXX;
+          rectY = nodeYY - (size * 0.9);
+          rectWidth = nodeWidth * inParams[0].length / allInsLength;
+          rectHeight = size * 0.9;
           context.rect(
-            nodeXX,
-            nodeYY - (size * 0.9),
-            nodeWidth * inParams[0].length / allInsLength,
-            size * 0.9
+            rectX,
+            rectY,
+            rectWidth,
+            rectHeight
           );
           context.closePath();
           context.fill();
@@ -95,12 +99,17 @@ sigma.canvas.nodes.def = (function() {
             nodeXX + (nodeWidth * inParams[0].length / allInsLength) * 0.5,
             nodeYY - (size * 0.2)
           );
+
         } else if (i == 1){
+            rectX = nodeXX + ( nodeWidth * inParams[i-1].length / allInsLength );
+            rectY = nodeYY - (size * 0.9);
+            rectWidth = nodeWidth * inParams[1].length / allInsLength;
+            rectHeight = size * 0.9;
             context.rect(
-              nodeXX + ( nodeWidth * inParams[i-1].length / allInsLength ),
-              nodeYY - (size * 0.9),
-              nodeWidth * inParams[1].length / allInsLength,
-              size * 0.9
+              rectX,
+              rectY,
+              rectWidth,
+              rectHeight
             );
             context.closePath();
             context.fill();
@@ -112,15 +121,19 @@ sigma.canvas.nodes.def = (function() {
               nodeYY - (size * 0.2)
             );
           }
-          else {
+          else  if (i > 1){
             for (j = 0; j < i-1; j++){
               temp += nodeWidth * inParams[j].length / allInsLength; // add width of jst rect
             }
+            rectX = nodeXX + temp + (nodeWidth * inParams[i-1].length / allInsLength );
+            rectY = nodeYY - (size * 0.9);
+            rectWidth = nodeWidth * inParams[i].length / allInsLength;
+            rectHeight = size * 0.9;
             context.rect(
-              nodeXX + temp + (nodeWidth * inParams[i-1].length / allInsLength ),
-              nodeYY - (size * 0.9),
-              nodeWidth * inParams[i].length / allInsLength,
-              size * 0.9
+              rectX,
+              rectY,
+              rectWidth,
+              rectHeight
             );
             context.closePath();
             context.fill();
@@ -132,15 +145,25 @@ sigma.canvas.nodes.def = (function() {
               nodeYY - (size * 0.2)
             );
           }
+          if (inParamsZones.length ==0)
+           node.inParamsZones.push(
+            rectX,
+            rectY,
+            rectX + rectHeight,
+            rectY + rectHeight
+          );
     }
+    
 
     // draw bottom rect(s)
     
-    var temp = 0;
+    var temp = 0,
+        tarr = [];
     
 
     for (i = 0; i < outParams.length; i++){
         temp = 0;
+        tarr = [];
         context.fillStyle = node.color;
         context.beginPath();
         if (i == 0) {
@@ -159,6 +182,13 @@ sigma.canvas.nodes.def = (function() {
             nodeXX + (nodeWidth * outParams[0].length / allOutsLength) * 0.5,
             nodeYY + nodeHeight + 10.5
           );
+
+          tarr.push(
+            nodeXX,
+            nodeYY + nodeHeight,
+            nodeXX + (nodeWidth * outParams[0].length / allOutsLength),
+            nodeYY + nodeHeight + (size * 0.9)
+          );
         } else if (i == 1){
             context.rect(
               nodeXX + ( nodeWidth * outParams[i-1].length / allOutsLength ),
@@ -175,6 +205,14 @@ sigma.canvas.nodes.def = (function() {
               nodeXX + ( nodeWidth * outParams[i-1].length / allOutsLength ) + (nodeWidth * outParams[1].length / allOutsLength) * 0.5,
               nodeYY + nodeHeight + 10.5
             );
+
+            tarr.push(
+              nodeXX + (nodeWidth * outParams[i-1].length / allOutsLength ),
+              nodeYY + nodeHeight,
+              nodeXX + ( nodeWidth * outParams[i-1].length / allOutsLength ) + (nodeWidth * outParams[1].length / allOutsLength),
+              nodeYY + nodeHeight + (size * 0.9)
+            );
+          
           }
           else {
             for (j = 0; j < i-1; j++){
@@ -195,7 +233,15 @@ sigma.canvas.nodes.def = (function() {
               nodeXX + temp + (nodeWidth * outParams[i-1].length / allOutsLength) + ((nodeWidth * outParams[i].length / allOutsLength)*0.5),
               nodeYY+ nodeHeight + 10.5
             );
+            tarr.push(
+              nodeXX + temp + (nodeWidth * outParams[i-1].length / allOutsLength ),
+              nodeYY + nodeHeight,
+              nodeXX + temp + (nodeWidth * outParams[i-1].length / allOutsLength ) + (nodeWidth * outParams[1].length / allOutsLength),
+              nodeYY + nodeHeight + (size * 0.9)
+            );
           }
+      //if (outParamsZones.length < outParams.length)
+        outParamsZones[i] = tarr;
     }
 
     // draw image
@@ -210,6 +256,45 @@ sigma.canvas.nodes.def = (function() {
 
   return renderer;
 }) ();
+
+
+//////////////////////////////////////////
+/////  creating new edge renderer
+///////////////////////////////////////////
+sigma.canvas.edges.def = function(edge, source, target, context, settings) {
+  var color = edge.color,
+      prefix = settings('prefix') || '',
+      edgeColor = settings('edgeColor'),
+      defaultNodeColor = settings('defaultNodeColor'),
+      defaultEdgeColor = settings('defaultEdgeColor');
+
+  if (!color)
+    switch (edgeColor) {
+      case 'source':
+        color = source.color || defaultNodeColor;
+        break;
+      case 'target':
+        color = target.color || defaultNodeColor;
+        break;
+      default:
+        color = defaultEdgeColor;
+        break;
+    }
+
+  context.strokeStyle = color;
+  context.lineWidth = edge[prefix + 'size'] || 1;
+  context.beginPath();
+  context.moveTo(
+    source[prefix + 'x'],
+    source[prefix + 'y']
+  );
+
+  context.lineTo(
+    target[prefix + 'x'],
+    target[prefix + 'y']
+  );
+  context.stroke();
+};
 
 //////////////////////////////////////////
 /////  creating new label renderer for previously defined shape renderer
@@ -234,7 +319,8 @@ sigma.canvas.labels.def = function(node, context, settings) {
 };
     
 
-    var id = 0,
+    var idN = 0,
+        idE = 0,
         g = {
           nodes: [],
           edges: []
@@ -253,54 +339,65 @@ sigma.canvas.labels.def = function(node, context, settings) {
         case 1:
         s.graph.addNode ({
           // Main attributes:
-          id: 'n' + id,
-          label: 'ahjbfgvjhgfvbhbvjfhbvkfhbvjfbvdfhkbvfghbv' + id,
-          x: 0 + id/5,
-          y: 0+ id/5,
+          id: 'n' + idN,
+          label: 'ahjbfgvjhgfvbhbvjfhbvkfhbvjfbvdfhkbvfghbv' + idN,
+          x: 0 + idN/5,
+          y: 0 + idN/5,
+          coordinates: [],
           size: 15,
           color: '#f5ceca',
           url: 'img1',
           forceLabel: true,
-          inParams: ['in1', 'i', 'innn3'],
+          inParams: [],
+          inParamsZones: [],
           outParams: ['out11111', 'out2', 'o3', 'ooo4', '5'],
+          outParamsZones: [],
         }); 
+        var arr = [];
+        
         break
 
         case 2:
         s.graph.addNode ({
           // Main attributes:
-          id: 'n' + id,
-          label: 'nodeeee' + id,
-          x: 0 + id/5,
-          y: 0+ id/5,
+          id: 'n' + idN,
+          label: 'nodeeee' + idN,
+          x: 0 + idN/5,
+          y: 0 + idN/5,
+          coordinates: [],
           size: 15,
           color: '#dfcde0',
           url: 'img2',
-          inParams: ['in1', 'in2', 'in3'],
+          inParams: [],
+          inParamsZones: [],
           outParams: ['out1', 'out22222'],
+          outParamsZones: []
         });
         break
 
         case 3:
         s.graph.addNode ({
           // Main attributes:
-          id: 'n' + id,
-          label: 'n' + id,
-          x: 0 + id/5,
-          y: 0+ id/5,
+          id: 'n' + idN,
+          label: 'n' + idN,
+          x: 0 + idN/5,
+          y: 0 + idN/5,
+          coordinates: [],
           size: 15,
           color: '#e5f98d',
           url: 'img3',
-          inParams: ['in1111111', 'innnnnnnn2', 'in33', '4444444444444444444444444444444444444444444444'],
+          inParams: [],
+          inParamsZones: [],
           outParams: ['out1'],
+          outParamsZones: [],
         });
         break
       }
-      console.log("Added: ", s.graph.nodes()[id]);
-      id++;
+      console.log("Added ", s.graph.nodes()[idN]);
+      idN++; 
       
       // this block is needed because of canvas bug - text in 'inParams' and 'outParams' will behave is strange way without this code
-      if (id == 1)
+      if (idN == 1)
       {
         s.graph.addNode ({
           // Main attributes:
@@ -308,11 +405,14 @@ sigma.canvas.labels.def = function(node, context, settings) {
           label: ' ',
           x: 0,
           y: 0,
+          coordinates: [],
           size: 15,
           color: '#e5f98d',
           url: 'img3',
-          inParams: ['in1', 'in2', 'in3', 'in4'],
-          outParams: ['out1'],
+          inParams: [],
+          inParamsZones: [],
+          outParams: [],
+          outParamsZones: []
         });
         s.refresh();
         s.graph.dropNode(1488);
@@ -341,12 +441,68 @@ sigma.canvas.labels.def = function(node, context, settings) {
 
     //get the popup box
     var modal = document.getElementById('myModal');
+    var drawingEdge = false;
 
     var dragListener = sigma.plugins.dragNodes(s, s.renderers[0]);
     dragListener.bind('drop', function(event) {
-      console.log('Moved: ', event['data']['node']['id'], ' to x =', event['data']['node']['x'], '  y =', event['data']['node']['y']);
+      console.log('Now: x ', s.graph.nodes()[0]);
+      drawingEdge = false;
     });
 
+    
+    function ifOnOutParamsZones(canvas, evt)
+    {
+      var rect = canvas.getBoundingClientRect();
+      var x = evt.clientX - rect.left,
+          y = evt.clientY - rect.top;
+      for (var node = 0; node < s.graph.nodes().length; node++)
+      {
+        for (var zone = 0; zone < s.graph.nodes()[node].outParamsZones.length; zone++)
+        {
+          if(x > s.graph.nodes()[node].outParamsZones[zone][0] && x < s.graph.nodes()[node].outParamsZones[zone][2] && 
+            y > s.graph.nodes()[node].outParamsZones[zone][1] && y < s.graph.nodes()[node].outParamsZones[zone][3])
+          {
+              return [true, node, zone, x, y];
+          }
+        }
+      }
+      return [false];
+    } 
+    function ifOnNode(canvas, evt)
+    {
+      var rect = canvas.getBoundingClientRect();
+      var x = evt.clientX - rect.left,
+          y = evt.clientY - rect.top;
+      for (var node = 0; node < s.graph.nodes().length; node++)
+      {
+        if(x > s.graph.nodes()[node].coordinates[0] && x < s.graph.nodes()[node].coordinates[2] && 
+          y > s.graph.nodes()[node].coordinates[1] && y < s.graph.nodes()[node].coordinates[3])
+        {
+            return [true, node];
+        }
+      }
+      return [false];
+    } 
+    var canvas = document.getElementsByClassName('sigma-mouse')[0];
+
+    canvas.addEventListener('click', function(evt) {
+        if (ifOnOutParamsZones(canvas, evt)[0] == true && drawingEdge == false)
+        {
+          
+          
+            drawingEdge = true;                      
+            document.body.style.cursor = 'crosshair';
+          
+          
+        }
+        else if (ifOnNode(canvas, evt)[0] == true && drawingEdge == true)
+          {
+            drawingEdge = false;
+            document.body.style.cursor = 'default';
+          }
+      }, true);
+
+    // Open modal
     s.bind('doubleClickNode', function(e) {
       modal.style.display = "block"; //show modal
       console.log(e.type, e.data.node.label, e.data.captor);
@@ -360,11 +516,15 @@ sigma.canvas.labels.def = function(node, context, settings) {
     }
 
     s.bind('overNode', function(e) {
-      document.body.style.cursor = 'move';
+      if (drawingEdge == false ) document.body.style.cursor = 'move';
     });
     s.bind('outNode', function(e) {
-      document.body.style.cursor = 'default';
+      if (drawingEdge == false ) document.body.style.cursor = 'default';
     });
 
+    
+    
+
+    
     CustomShapes.init(s);
     s.refresh();
