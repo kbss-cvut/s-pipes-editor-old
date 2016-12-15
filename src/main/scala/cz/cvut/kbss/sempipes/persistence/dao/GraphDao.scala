ecbea49a068a4f4fd7acb6fa3cc840fcf33c67b6
YanDoroshenko <yan1994doroshenko@gmail.com>
2016-12-15 23:38:05 +0100

    Fixed not closed transaction issue, added get all graphs method

--- src/main/scala/cz/cvut/kbss/sempipes/persistence/dao/GraphDao.scala
+++ src/main/scala/cz/cvut/kbss/sempipes/persistence/dao/GraphDao.scala
@@ -2,15 +2,11 @@ package cz.cvut.kbss.sempipes.persistence.dao
 
 import java.net.URI
 
-import cz.cvut.kbss.jopa.model.EntityManagerFactory
-import cz.cvut.kbss.sempipes.model.graph.{Edge, Graph, Node}
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.context.annotation.Bean
+import cz.cvut.kbss.sempipes.model.Vocabulary
+import cz.cvut.kbss.sempipes.model.graph.Graph
 import org.springframework.stereotype.Repository
 
 import scala.collection.JavaConverters._
-import scala.collection.mutable
-import scala.collection.mutable.{Set => MutableSet}
 
 /**
   * Created by Yan Doroshenko (yandoroshenko@protonmail.com) on 03.11.16.
@@ -31,15 +27,29 @@ class GraphDao extends BaseDao[Graph] {
     }
   }
 
-  def getAll(): Traversable[Graph] =
-    List()
+  def getAll(): Option[Traversable[Graph]] = {
+    val em = emf.createEntityManager()
+    try {
+      em.createNativeQuery("select ?s ?p ?o where { ?s a <http://onto.fel.cvut.cz/ontologies/sempipes/graph> }", classOf[Graph])
+        //.setParameter("type", Vocabulary.s_c_graph)
+        .getResultList() match {
+        case nonEmpty: java.util.List[Graph] if !nonEmpty.isEmpty =>
+          Some(nonEmpty.asScala)
+        case empty: java.util.List[Graph] if empty.isEmpty =>
+          None
+      }
+    }
+    finally {
+      em.close()
+    }
+  }
 
   override def delete(uri: URI): Option[URI] = {
     val em = emf.createEntityManager()
-    em.getTransaction().begin()
     try {
       em.find(classOf[Graph], uri) match {
         case n: Graph =>
+          em.getTransaction().begin()
           em.remove(n)
           em.getTransaction().commit()
           Some(uri)
