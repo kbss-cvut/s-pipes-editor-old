9f707e7ff1d7a61b36928486e518e4ad2d4a9b53
YanDoroshenko <yan1994doroshenko@gmail.com>
2016-12-15 18:06:14 +0100

    Refactoring

--- src/main/scala/cz/cvut/kbss/sempipes/persistence/PersistenceFactory.scala
+++ src/main/scala/cz/cvut/kbss/sempipes/persistence/PersistenceFactory.scala
@@ -4,16 +4,15 @@ import javax.annotation.PostConstruct
 
 import cz.cvut.kbss.jopa.Persistence
 import cz.cvut.kbss.jopa.model.JOPAPersistenceProperties._
+import cz.cvut.kbss.jopa.model.PersistenceProperties.JPA_PERSISTENCE_PROVIDER
 import cz.cvut.kbss.jopa.model.{EntityManagerFactory, JOPAPersistenceProvider}
 import cz.cvut.kbss.ontodriver.config.OntoDriverProperties
-import cz.cvut.kbss.jopa.model.PersistenceProperties.JPA_PERSISTENCE_PROVIDER
+import cz.cvut.kbss.sempipes.util.ConfigParam._
+import cz.cvut.kbss.sempipes.util.Constants
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.{Bean, Configuration, Primary, PropertySource}
 import org.springframework.core.env.Environment
-import cz.cvut.kbss.sempipes.util.ConfigParam._
-import cz.cvut.kbss.sempipes.util.Constants
 
-import scala.beans.BeanProperty
 import scala.collection.JavaConverters._
 
 /**
@@ -38,7 +37,7 @@ class PersistenceFactory {
     val properties = DEFAULT_PARAMS +
       (ONTOLOGY_PHYSICAL_URI_KEY -> environment.getProperty(REPOSITORY_URL.toString)) +
       (DATA_SOURCE_CLASS -> environment.getProperty(DRIVER.toString))
-    emf = Persistence.createEntityManagerFactory("rtPU", properties.asJava)
+    emf = Persistence.createEntityManagerFactory("persistenceFactory", properties.asJava)
   }
 
   private def initParams = {
