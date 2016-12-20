5cc0657f1101f0fe0252dce9014eb89a911d6d45
YanDoroshenko <yan1994doroshenko@gmail.com>
2016-12-20 23:35:51 +0100

    Tests fixed

--- src/test/java/PersistenceTest.java
+++ src/test/java/PersistenceTest.java
@@ -1,11 +1,11 @@
 import cz.cvut.kbss.sempipes.config.PersistenceConfig;
 import cz.cvut.kbss.sempipes.config.RestConfig;
-import cz.cvut.kbss.sempipes.persistence.dao.EdgeDao;
-import cz.cvut.kbss.sempipes.persistence.dao.GraphDao;
-import cz.cvut.kbss.sempipes.persistence.dao.NodeDao;
 import cz.cvut.kbss.sempipes.model.graph.Edge;
 import cz.cvut.kbss.sempipes.model.graph.Graph;
 import cz.cvut.kbss.sempipes.model.graph.Node;
+import cz.cvut.kbss.sempipes.persistence.dao.EdgeDao;
+import cz.cvut.kbss.sempipes.persistence.dao.GraphDao;
+import cz.cvut.kbss.sempipes.persistence.dao.NodeDao;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +17,8 @@ import scala.Some;
 import java.net.URI;
 import java.util.HashSet;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * Created by Yan Doroshenko (yandoroshenko@protonmail.com) on 03.12.16.
@@ -41,7 +42,7 @@ public class PersistenceTest {
 
     @Test
     public void edgePersistenceTest() throws Exception {
-        URI uri = new URI("https://uri");
+        URI uri = new URI("https://edges/1");
         edgeDao.delete(uri);
         assertEquals(scala.None$.MODULE$, edgeDao.get(uri));
         final HashSet<String> types = new HashSet<>();
@@ -49,18 +50,18 @@ public class PersistenceTest {
         types.add("https://type/2");
         types.add("https://type/3");
         types.add("https://type/4");
-        final Node n = new Node(new URI("https://uri" + nodeCount++), "Label", 1, 2, types, new java.util.HashSet<String>(), new java.util.HashSet<String>());
-        final Node n1 = new Node(new URI("https://uri" + nodeCount++), "Label", 1, 2, types, new java.util.HashSet<String>(), new java.util.HashSet<String>());
+        final Node n = new Node(new URI("https://nodes/" + nodeCount++), "Label", 1, 2, types, new java.util.HashSet<String>(), new java.util.HashSet<String>());
+        final Node n1 = new Node(new URI("https://nodes/" + nodeCount++), "Label", 1, 2, types, new java.util.HashSet<String>(), new java.util.HashSet<String>());
         Edge e = new Edge(uri, n, n1);
         assertEquals(new Some<>(e), edgeDao.add(e));
-        assertEquals(new Some<>(e), edgeDao.get(new URI("https://uri")));
+        assertEquals(new Some<>(e), edgeDao.get(new URI("https://edges/1")));
         assertEquals(new Some<>(uri), edgeDao.delete(uri));
         assertEquals(scala.None$.MODULE$, edgeDao.get(uri));
     }
 
     @Test
     public void nodePersistenceTest() throws Exception {
-        URI uri = new URI("https://uri" + nodeCount);
+        URI uri = new URI("https://nodes/" + nodeCount);
         nodeDao.delete(uri);
         assertEquals(scala.None$.MODULE$, nodeDao.get(uri));
         Node n = persistNode();
@@ -72,15 +73,13 @@ public class PersistenceTest {
 
     @Test
     public void graphPersistenceTest() throws Exception {
-        final URI uri = new URI("https://uri");
-        graphDao.delete(uri);
-        assertEquals(scala.None$.MODULE$, graphDao.get(uri));
+        final URI uri = new URI("https://graphs/1");
         final HashSet<String> types = new HashSet<>();
         types.add("https://type/1");
         types.add("https://type/2");
         types.add("https://type/3");
-        final Node n = new Node(new URI("https://uri" + nodeCount), "Label", 1, 2, types, new java.util.HashSet<String>(), new java.util.HashSet<String>());
-        final Edge e = new Edge(new URI("https://edge"), n, n);
+        final Node n = new Node(new URI("https://nodes/1" + nodeCount), "Label", 1, 2, types, new java.util.HashSet<String>(), new java.util.HashSet<String>());
+        final Edge e = new Edge(new URI("https://edges/1"), n, n);
         final HashSet<Node> nodes = new HashSet<>();
         nodes.add(n);
         final HashSet<Edge> edges = new HashSet<>();
@@ -89,24 +88,21 @@ public class PersistenceTest {
         assertEquals(new Some<>(g), graphDao.add(g));
         assertNotNull(graphDao.get(uri));
         assertEquals(new Some<>(g), graphDao.get(uri));
+        assertEquals(g.getNodes(), graphDao.get(uri).get().getNodes());
         assertEquals(new Some<>(uri), graphDao.delete(uri));
         assertEquals(scala.None$.MODULE$, graphDao.get(uri));
     }
 
     @Test
     public void getAllGraphsTest() throws Exception {
-        final URI uri1 = new URI("https://uri3");
-        final URI uri2 = new URI("https://uri4");
-        graphDao.delete(uri1);
-        graphDao.delete(uri2);
-        assertEquals(scala.None$.MODULE$, graphDao.get(uri1));
-        assertEquals(scala.None$.MODULE$, graphDao.get(uri2));
+        final URI uri1 = new URI("https://graphs/1");
+        final URI uri2 = new URI("https://graphs/2");
         final HashSet<String> types = new HashSet<>();
         types.add("https://type/1");
         types.add("https://type/2");
         types.add("https://type/3");
-        final Node n = new Node(new URI("https://uri" + nodeCount), "Label", 1, 2, types, new java.util.HashSet<String>(), new java.util.HashSet<String>());
-        final Edge e = new Edge(new URI("https://edge"), n, n);
+        final Node n = new Node(new URI("https://nodes/1" + nodeCount), "Label", 1, 2, types, new java.util.HashSet<String>(), new java.util.HashSet<String>());
+        final Edge e = new Edge(new URI("https://nodes/1"), n, n);
         final HashSet<Node> nodes = new HashSet<>();
         nodes.add(n);
         final HashSet<Edge> edges = new HashSet<>();
@@ -117,15 +113,14 @@ public class PersistenceTest {
         assertNotNull(graphDao.get(uri1));
         assertEquals(new Some<>(g2), graphDao.add(g2));
         assertNotNull(graphDao.get(uri2));
-        assertEquals(2, graphDao.getAll().get().size());
-        assertEquals(g1, graphDao.getAll().get().head());
-        assertEquals(g1, graphDao.getAll().get().drop(1).head());
+        int size =  graphDao.getAll().get().size();
+        Graph readGraph1 = graphDao.getAll().get().last();
         assertEquals(new Some<>(uri1), graphDao.delete(uri1));
         assertEquals(scala.None$.MODULE$, graphDao.get(uri1));
-        assertEquals(1, graphDao.getAll().get().size());
-        assertEquals(g2, graphDao.getAll().get().head());
+        assertEquals(size - 1, graphDao.getAll().get().size());
         assertEquals(new Some<>(uri2), graphDao.delete(uri2));
         assertEquals(scala.None$.MODULE$, graphDao.get(uri2));
+        assertEquals(size - 2, graphDao.getAll().get().size());
     }
 
     private Node persistNode() throws Exception {
@@ -134,7 +129,7 @@ public class PersistenceTest {
         types.add("https://type/2");
         types.add("https://type/3");
         types.add("https://type/4");
-        final Node n = new Node(new URI("https://uri" + nodeCount++), "Label", 1, 2, types, new java.util.HashSet<String>(), new java.util.HashSet<String>());
+        final Node n = new Node(new URI("https://nodes/" + nodeCount++), "Label", 1, 2, types, new java.util.HashSet<String>(), new java.util.HashSet<String>());
         nodeDao.add(n);
         return n;
     }
--- src/test/java/RestTest.java
+++ src/test/java/RestTest.java
@@ -1,32 +1,27 @@
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.github.jsonldjava.core.JsonLdProcessor;
-import com.github.jsonldjava.utils.JsonUtils;
-import cz.cvut.kbss.jsonld.JsonLd;
 import cz.cvut.kbss.jsonld.deserialization.JsonLdDeserializer;
+import cz.cvut.kbss.sempipes.config.PersistenceConfig;
 import cz.cvut.kbss.sempipes.config.RestConfig;
-import cz.cvut.kbss.sempipes.persistence.dao.GraphDao;
+import cz.cvut.kbss.sempipes.model.graph.Edge;
+import cz.cvut.kbss.sempipes.model.graph.Graph;
 import cz.cvut.kbss.sempipes.model.graph.Node;
-import cz.cvut.kbss.sempipes.persistence.dao.NodeDao;
+import cz.cvut.kbss.sempipes.persistence.dao.GraphDao;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
-import org.junit.experimental.categories.Categories;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.net.URI;
-import java.util.List;
+import java.util.HashSet;
 
 import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
@@ -34,7 +29,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  */
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = RestConfig.class)
+@ContextConfiguration(classes = {RestConfig.class, PersistenceConfig.class})
 @WebAppConfiguration
 public class RestTest {
 
@@ -42,7 +37,7 @@ public class RestTest {
     private WebApplicationContext webApplicationContext;
 
     @Autowired
-    private NodeDao nodeDao;
+    private GraphDao graphDao;
 
     private MockMvc mockMvc;
 
@@ -55,50 +50,34 @@ public class RestTest {
     }
 
     @Test
-    public void getTest() throws Exception {
-        mockMvc.perform(get("/")).andExpect(status().isOk());
+    public void getAllGraphsTest() throws Exception {
+        ResultActions result = mockMvc.perform(get("/graphs/"));
+        if (graphDao.getAll().isEmpty()) {
+            result.andExpect(status().isNoContent());
+            assertEquals("[]", result.andReturn().getResponse().getContentAsString());
+        } else {
+            result.andExpect(status().isOk());
+        }
     }
 
     @Test
-    @Ignore
-    public void serializeNodeTest() throws Exception {
-        MvcResult result = mockMvc.perform(get("/")).andExpect(status().isOk()).andReturn();
-        String jsonLd = result.getResponse().getContentAsString();
-        /*Node expectedNode = nodeDao.get(new URI("/newNode"));
-
-        ObjectMapper mapper = new ObjectMapper();
-        String expectedString = mapper.writeValueAsString(expectedNode);
-        assertEquals(jsonLd, expectedString);
-
-        Object jsonLdObject = JsonUtils.fromString(jsonLd);
-        Object expectedObject = JsonUtils.fromString(expectedString);
-
-        assertEquals(jsonLdObject, expectedObject);*/
-    }
-
-    @Test
-    @Ignore
-    public void deserializeNodeTest() throws Exception {
-        MvcResult result = mockMvc.perform(get("/")).andExpect(status().isOk()).andReturn();
-        String jsonLd = result.getResponse().getContentAsString();
-
-        Object jsonLdObject = JsonUtils.fromString(jsonLd);
-        List<Object> expanded = JsonLdProcessor.expand(jsonLdObject);
-        deserializer.deserialize(expanded, Node.class);
-    }
-
-    @Test
-    @Ignore
-    public void createNodeTest() throws Exception {
-        mockMvc.perform(post("/nodes").contentType(JsonLd.MEDIA_TYPE).content("[]")).andExpect(status().isCreated());
-    }
-
-    @Test
-    @Ignore
-    public void loadGraphDummyTest() throws Exception {
-        MvcResult result = mockMvc.perform(get("/graphs/123")).andExpect(status().isOk()).andReturn();
-        String response = result.getResponse().getContentAsString();
-        String expected = "{\"uri\":\"/graphs/123\",\"label\":\"Label\",\"nodes\":[],\"edges\":[]}";
-        assertEquals(expected, response);
+    public void getGraphTest() throws Exception {
+        final HashSet<String> types = new HashSet<>();
+        types.add("https://type/1");
+        types.add("https://type/2");
+        types.add("https://type/3");
+        final Node n = new Node(new URI("https://nodes/1"), "Label", 1, 2, types, new java.util.HashSet<>(), new java.util.HashSet<String>());
+        final Edge e = new Edge(new URI("https://edges/1"), n, n);
+        final HashSet<Node> nodes = new HashSet<>();
+        nodes.add(n);
+        final HashSet<Edge> edges = new HashSet<>();
+        edges.add(e);
+        final Graph g1 = new Graph(new URI("https://graphs/1"), "Graph", nodes, edges);
+        graphDao.delete(g1.getUri());
+        graphDao.add(g1);
+        ResultActions result = mockMvc.perform(get("/graphs/1"));
+        result.andExpect(status().isOk());
+        assertEquals("{\"uri\":\"https://graphs/1\",\"label\":\"Graph\",\"nodes\":[{\"uri\":\"https://nodes/1\",\"label\":\"Label\",\"x\":1.0,\"y\":2.0,\"nodeTypes\":[\"https://type/2\",\"https://type/3\",\"https://type/1\"],\"inParameters\":[],\"outParameters\":[]}],\"edges\":[{\"uri\":\"https://edges/1\",\"sourceNode\":{\"uri\":\"https://nodes/1\",\"label\":\"Label\",\"x\":1.0,\"y\":2.0,\"nodeTypes\":[\"https://type/2\",\"https://type/3\",\"https://type/1\"],\"inParameters\":[],\"outParameters\":[]},\"destinationNode\":{\"uri\":\"https://nodes/1\",\"label\":\"Label\",\"x\":1.0,\"y\":2.0,\"nodeTypes\":[\"https://type/2\",\"https://type/3\",\"https://type/1\"],\"inParameters\":[],\"outParameters\":[]}}]}",result.andReturn().getResponse().getContentAsString());
+        graphDao.delete(g1.getUri());
     }
 }
