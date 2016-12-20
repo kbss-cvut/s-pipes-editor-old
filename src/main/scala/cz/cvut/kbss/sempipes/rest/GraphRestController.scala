3fba8a41a2c61de429750d87b17b1ed213090e82
YanDoroshenko <yan1994doroshenko@gmail.com>
2016-12-20 23:36:58 +0100

    Added few practically applicable mappings

--- src/main/scala/cz/cvut/kbss/sempipes/rest/GraphRestController.scala
+++ src/main/scala/cz/cvut/kbss/sempipes/rest/GraphRestController.scala
@@ -3,9 +3,8 @@ package cz.cvut.kbss.sempipes.rest
 import java.net.URI
 
 import cz.cvut.kbss.jsonld.JsonLd
-import cz.cvut.kbss.sempipes.persistence.dao.{EdgeDao, GraphDao, NodeDao}
 import cz.cvut.kbss.sempipes.model.graph.{Edge, Graph, Node}
-import org.apache.http.HttpResponse
+import cz.cvut.kbss.sempipes.persistence.dao.{EdgeDao, GraphDao, NodeDao}
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.{HttpStatus, ResponseEntity}
 import org.springframework.web.bind.annotation._
@@ -17,6 +16,7 @@ import scala.collection.JavaConverters._
   * Created by Yan Doroshenko (yandoroshenko@protonmail.com) on 28.10.16.
   */
 @RestController
+@RequestMapping(path = Array("/graphs/"))
 class GraphRestController {
 
   @BeanProperty
@@ -32,17 +32,42 @@ class GraphRestController {
   var edgeDao: EdgeDao = _
 
   @GetMapping(path = Array("/"), produces = Array(JsonLd.MEDIA_TYPE))
-  def login = new ResponseEntity(new Node(new URI("https://uri"), "L", 1, 1, Set("type").asJava, Set("in").asJava, Set("out").asJava), HttpStatus.OK)
+  def getAllGraphs: ResponseEntity[java.util.List[Graph]] =
+    graphDao.getAll() match {
+      case Some(graphs) =>
+        new ResponseEntity(graphs.toBuffer.asJava, HttpStatus.OK)
+      case None =>
+        new ResponseEntity(Seq[Graph]().asJava, HttpStatus.NOT_FOUND)
+    }
 
-  @GetMapping(path = Array("/admin/"))
-  def admin = new ResponseEntity("Permission granted to admin", HttpStatus.OK)
+  @GetMapping(path = Array("/edges/{uri}"), produces = Array(JsonLd.MEDIA_TYPE))
+  def loadEdge(@PathVariable uri: String): ResponseEntity[Edge] = {
+    val edge = edgeDao.get(new URI("https://edges/" + uri))
+    edge match {
+      case Some(e) =>
+        new ResponseEntity(e, HttpStatus.OK)
+      case None =>
+        new ResponseEntity(new Edge(), HttpStatus.NOT_FOUND)
+    }
+  }
 
-  @GetMapping(path = Array("/graphs/{relativeUri}"), produces = Array(JsonLd.MEDIA_TYPE))
-  def loadGraph(@PathVariable relativeUri: String): ResponseEntity[Any] =
-    graphDao.get(new URI("https://graphs/" + relativeUri)) match {
+  @GetMapping(path = Array("/{uri}"), produces = Array(JsonLd.MEDIA_TYPE))
+  def loadGraph(@PathVariable uri: String): ResponseEntity[Graph] = {
+    val graph = graphDao.get(new URI("https://graphs/" + uri))
+    graph match {
       case Some(g) =>
         new ResponseEntity(g, HttpStatus.OK)
       case None =>
-        new ResponseEntity[Any](None, HttpStatus.NOT_FOUND)
+        new ResponseEntity(new Graph(), HttpStatus.NOT_FOUND)
+    }
+  }
+
+  @GetMapping(path = Array("/{uri}/nodes"), produces = Array(JsonLd.MEDIA_TYPE))
+  def loadGraphNodes(@PathVariable uri: String): ResponseEntity[java.util.Set[Node]] =
+    graphDao.get(new URI("https://graphs/" + uri)) match {
+      case Some(g) =>
+        new ResponseEntity(g.getNodes(), HttpStatus.OK)
+      case None =>
+        new ResponseEntity(Set[Node]().asJava, HttpStatus.NOT_FOUND)
     }
 }
