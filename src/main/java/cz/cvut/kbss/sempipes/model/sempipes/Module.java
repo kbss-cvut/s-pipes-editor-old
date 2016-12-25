61bd26e3f778ff2dba07ebdb381e0f9fa2efa72a
YanDoroshenko <yan1994doroshenko@gmail.com>
2016-12-25 23:00:38 +0200

    Added Module entity template

---
+++ src/main/java/cz/cvut/kbss/sempipes/model/sempipes/Module.java
@@ -0,0 +1,43 @@
+package cz.cvut.kbss.sempipes.model.sempipes;
+
+import cz.cvut.kbss.jopa.model.annotations.Id;
+import cz.cvut.kbss.jopa.model.annotations.OWLClass;
+import cz.cvut.kbss.jopa.model.annotations.OWLDataProperty;
+import cz.cvut.kbss.sempipes.model.Vocabulary;
+
+import java.net.URI;
+
+/**
+ * Created by Yan Doroshenko (yandoroshenko@protonmail.com) on 22.12.16.
+ */
+@OWLClass(iri = Vocabulary.s_c_module)
+public class Module {
+    @Id
+    private URI uri;
+    @OWLDataProperty(iri = Vocabulary.s_p_label)
+    private String label;
+
+    public Module() {
+    }
+
+    public Module(URI uri, String label) {
+        this.uri = uri;
+        this.label = label;
+    }
+
+    public URI getUri() {
+        return uri;
+    }
+
+    public void setUri(URI uri) {
+        this.uri = uri;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+}
