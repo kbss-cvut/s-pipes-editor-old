2661825fc1024fcdc1e191b357669239ef43bb99
Miroslav Blasko <blcham@gmail.com>
2017-01-04 15:52:53 +0100

    [new] jopa utils for sesame access

---
+++ src/main/java/cz/cvut/kbss/sempipes/util/JopaPersistenceUtils.java
@@ -0,0 +1,18 @@
+package cz.cvut.kbss.sempipes.util;
+
+import cz.cvut.kbss.jopa.model.EntityManager;
+import org.openrdf.repository.Repository;
+
+/**
+ * Created by Miroslav Blasko on 4.1.17.
+ */
+public class JopaPersistenceUtils {
+
+    public static Repository getRepository(EntityManager entityManager) {
+        try {
+            return entityManager.unwrap(Repository.class);
+        } finally {
+            entityManager.close();
+        }
+    }
+}
