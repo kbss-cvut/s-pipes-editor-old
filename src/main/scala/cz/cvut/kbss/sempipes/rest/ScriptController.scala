3a78a03c216713f8fe35a33f5db1e18a5024dadc
YanDoroshenko <yan1994doroshenko@gmail.com>
2017-01-19 17:21:31 +0100

    [new] Added endpoint for retrieving all scripts [upd] Refactoring

--- src/main/scala/cz/cvut/kbss/sempipes/rest/ScriptController.scala
+++ src/main/scala/cz/cvut/kbss/sempipes/rest/ScriptController.scala
@@ -1,13 +1,14 @@
 package cz.cvut.kbss.sempipes.rest
 
 import cz.cvut.kbss.jsonld.JsonLd
-import cz.cvut.kbss.sempipes.model.sempipes.{Module, ModuleType}
+import cz.cvut.kbss.sempipes.model.sempipes.{Context, Module, ModuleType}
 import cz.cvut.kbss.sempipes.service.SempipesService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.PropertySource
 import org.springframework.core.env.Environment
 import org.springframework.http.{HttpStatus, ResponseEntity}
 import org.springframework.web.bind.annotation.{GetMapping, PathVariable, RequestMapping, RestController}
+import java.util.{Set => JSet}
 
 import scala.collection.JavaConverters._
 
@@ -25,21 +26,24 @@ class ScriptController {
   @Autowired
   private var service: SempipesService = _
 
-  private var SempipesLocation = "https://kbss.felk.cvut.cz/sempipes-sped/"
+  private var SempipesLocation = "https://kbss.felk.cvut.cz/sempipes-sped"
 
   @GetMapping(produces = Array(JsonLd.MEDIA_TYPE))
-  def getScripts = None
+  def getScripts: ResponseEntity[JSet[Context]] = service.getScripts(SempipesLocation + "/scripts") match {
+    case Some(contexts) => new ResponseEntity(contexts.toSet.asJava, HttpStatus.OK)
+    case None => new ResponseEntity(Set[Context]().asJava, HttpStatus.OK)
+  }
 
   @GetMapping(path = Array("/{id}"), produces = Array(JsonLd.MEDIA_TYPE))
-  def getScript(@PathVariable id: String) =
-    service.getScript(SempipesLocation + "contexts", id) match {
+  def getScript(@PathVariable id: String): ResponseEntity[AnyRef] =
+    service.getScript(SempipesLocation + "/contexts", id) match {
       case Some(context) => new ResponseEntity(context, HttpStatus.OK)
       case None => new ResponseEntity("Script with ID " + id + " is not found", HttpStatus.NOT_FOUND)
     }
 
   @GetMapping(path = Array("/{id}/moduleTypes"), produces = Array(JsonLd.MEDIA_TYPE))
-  def getModuleTypes(@PathVariable id: String): ResponseEntity[java.util.Set[ModuleType]] = {
-    service.getModuleTypes(SempipesLocation + "contexts/" + id) match {
+  def getModuleTypes(@PathVariable id: String): ResponseEntity[JSet[ModuleType]] = {
+    service.getModuleTypes(SempipesLocation + "/contexts/" + id) match {
       case Some(types) if types.nonEmpty =>
         new ResponseEntity(types.toSet.asJava, HttpStatus.OK)
       case _ =>
@@ -48,8 +52,8 @@ class ScriptController {
   }
 
   @GetMapping(path = Array("/{id}/modules"), produces = Array(JsonLd.MEDIA_TYPE))
-  def getModules(@PathVariable id: String): ResponseEntity[java.util.Set[Module]] = {
-    service.getModules(SempipesLocation + "contexts/" + id) match {
+  def getModules(@PathVariable id: String): ResponseEntity[JSet[Module]] = {
+    service.getModules(SempipesLocation + "/contexts/" + id) match {
       case Some(modules) if modules.nonEmpty =>
         new ResponseEntity(modules.toSet.asJava, HttpStatus.OK)
       case _ =>
