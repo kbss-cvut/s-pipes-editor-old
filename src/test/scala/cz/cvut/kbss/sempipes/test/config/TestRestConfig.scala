a7333fc7bcee7d8261319f4884b523a83f993c64
YanDoroshenko <yan1994doroshenko@gmail.com>
2017-01-24 19:48:59 +0100

    [fix] Tests fixed and working

--- src/test/scala/cz/cvut/kbss/sempipes/test/config/TestRestConfig.scala
+++ src/test/scala/cz/cvut/kbss/sempipes/test/config/TestRestConfig.scala
@@ -1,10 +1,12 @@
 package cz.cvut.kbss.sempipes.test.config
 
 import org.springframework.context.annotation.{ComponentScan, Configuration}
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
 
 /**
   * Created by Yan Doroshenko (yandoroshenko@protonmail.com) on 21.01.17.
   */
 @Configuration
 @ComponentScan(basePackages = Array("cz.cvut.kbss.sempipes.rest"))
+@EnableWebMvc
 class TestRestConfig
