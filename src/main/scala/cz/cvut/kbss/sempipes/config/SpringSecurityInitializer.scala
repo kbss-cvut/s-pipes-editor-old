6370cf4b858b51436eee69a584e0bda48bc625e4
YanDoroshenko <yan1994doroshenko@gmail.com>
2017-02-03 13:17:32 +0100

    [upd] Dropped security for now to eliminate access issues

--- src/main/scala/cz/cvut/kbss/sempipes/config/SpringSecurityInitializer.scala
+++
@@ -1,8 +0,0 @@
-package cz.cvut.kbss.sempipes.config
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer
-
-/**
-  * Created by Yan Doroshenko (yandoroshenko@protonmail.com) on 11.12.16.
-  */
-class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer
