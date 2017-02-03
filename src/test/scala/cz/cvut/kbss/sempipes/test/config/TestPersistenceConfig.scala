5015b7aaf24d2c2a4e929ff1830aa496a9bb33da
YanDoroshenko <yan1994doroshenko@gmail.com>
2017-02-03 20:06:52 +0100

    [fix] Fixed config

--- src/test/scala/cz/cvut/kbss/sempipes/test/config/TestPersistenceConfig.scala
+++ src/test/scala/cz/cvut/kbss/sempipes/test/config/TestPersistenceConfig.scala
@@ -7,9 +7,6 @@ import org.springframework.context.annotation.{ComponentScan, Configuration, Imp
   * Created by Yan Doroshenko (yandoroshenko@protonmail.com) on 24.01.17.
   */
 @Configuration
-@ComponentScan(basePackages = Array("cz.cvut.kbss.sempipes.test.persistence"))
+@ComponentScan(basePackages = Array("cz.cvut.kbss.sempipes.persistence.dao"))
 @Import(Array(classOf[TestPersistenceFactory]))
-class TestPersistenceConfig {
-
-
-}
+class TestPersistenceConfig
