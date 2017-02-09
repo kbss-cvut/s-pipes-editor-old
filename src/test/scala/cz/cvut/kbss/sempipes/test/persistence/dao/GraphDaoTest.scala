9ae64eb85a75741105b2b8f456a04ebe32bf441e
Yan Doroshenko <yandoroshenko@protonmail.com>
2017-02-09 11:46:52 +0100

    [fix] Dao tests fixed with a temporary fix

--- src/test/scala/cz/cvut/kbss/sempipes/test/persistence/dao/GraphDaoTest.scala
+++ src/test/scala/cz/cvut/kbss/sempipes/test/persistence/dao/GraphDaoTest.scala
@@ -9,8 +9,6 @@ import org.junit.Assert._
 import org.junit.Test
 import org.springframework.beans.factory.annotation.Autowired
 
-import scala.collection.JavaConverters._
-
 /**
   * Created by Yan Doroshenko (yandoroshenko@protonmail.com) on 03.02.17.
   */
@@ -21,7 +19,7 @@ class GraphDaoTest extends BaseDaoTestRunner {
   private var graphDao: GraphDao = _
 
   @Test
-  def getAndAddGraph() {
+  def getAndAddGraph {
     val uri = new URI("https://graphs/11")
 
     //todo WrapperSet support
@@ -29,11 +27,13 @@ class GraphDaoTest extends BaseDaoTestRunner {
     types.add("https://type/1")
     types.add("https://type/2")
     types.add("https://type/3")
-
     val n = new Node(new URI("https://nodes/11" + nodeCount), "Label", 1, 2, types, new java.util.HashSet[String](), new java.util.HashSet[String]())
-    val e = new Edge(new URI("https://edges/11"), n, null)
-    val nodes = Set[Node](n, n, n).asJava
-    val edges = Set[Edge](e, e).asJava
+    val e = new Edge(new URI("https://edges/11"), n, n)
+    val nodes = new java.util.HashSet[Node]()
+    nodes.add(n)
+    val edges = new java.util.HashSet[Edge]()
+    edges.add(e)
+
     val g = new Graph(uri, "Graph", nodes, edges)
     assertEquals(Some(g), graphDao.add(g))
     assertNotNull(graphDao.get(uri))
@@ -44,18 +44,23 @@ class GraphDaoTest extends BaseDaoTestRunner {
   }
 
   @Test
-  def getAllGraphs() {
+  def getAllGraphs {
     val uri1 = new URI("https://graphs/1")
     val uri2 = new URI("https://graphs/2")
     val uri = new URI("https://graphs/11")
+
+    //todo WrapperSet support
     val types = new java.util.HashSet[String]()
     types.add("https://type/1")
     types.add("https://type/2")
     types.add("https://type/3")
-    val n = new Node(new URI("https://nodes/11" + nodeCount), "Label", 1, 2, types, Set[String]().asJava, Set[String]().asJava)
+    val n = new Node(new URI("https://nodes/11" + nodeCount), "Label", 1, 2, types, new java.util.HashSet[String](), new java.util.HashSet[String]())
     val e = new Edge(new URI("https://edges/11"), n, n)
-    val nodes = Set[Node]().asJava
-    val edges = Set[Edge]().asJava
+    val nodes = new java.util.HashSet[Node]()
+    nodes.add(n)
+    val edges = new java.util.HashSet[Edge]()
+    edges.add(e)
+
     val g1 = new Graph(uri1, "Graph", nodes, edges)
     val g2 = new Graph(uri2, "Graph", nodes, edges)
     assertEquals(Some(g1), graphDao.add(g1))
@@ -72,7 +77,7 @@ class GraphDaoTest extends BaseDaoTestRunner {
   }
 
   @Test
-  def deleteNonExistentGraph() {
+  def deleteNonExistentGraph {
     assertEquals(None, graphDao.delete(URI.create("https://graph/that/does/not/exist")))
   }
 }
