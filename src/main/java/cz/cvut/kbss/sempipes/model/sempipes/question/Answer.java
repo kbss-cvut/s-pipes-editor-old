19171acad278d288304ec741953c63fa35c81c38
Yan Doroshenko <yan1994doroshenko@gmail.com>
2017-02-11 18:56:27 +0100

    [new] Added QA model

---
+++ src/main/java/cz/cvut/kbss/sempipes/model/sempipes/question/Answer.java
@@ -0,0 +1,69 @@
+package cz.cvut.kbss.sempipes.model.sempipes.question;
+
+import cz.cvut.kbss.jopa.model.annotations.OWLClass;
+import cz.cvut.kbss.jopa.model.annotations.OWLDataProperty;
+import cz.cvut.kbss.jopa.model.annotations.OWLObjectProperty;
+import cz.cvut.kbss.sempipes.model.AbstractEntity;
+import cz.cvut.kbss.sempipes.model.Vocabulary;
+
+import java.net.URI;
+import java.util.UUID;
+
+/**
+ * Created by Yan Doroshenko (yandoroshenko@protonmail.com) on 2/11/17.
+ */
+@OWLClass(iri = Vocabulary.s_c_answer)
+public class Answer extends AbstractEntity {
+
+    @OWLDataProperty(iri = Vocabulary.s_p_has_data_value)
+    private String textValue;
+
+    @OWLObjectProperty(iri = Vocabulary.s_p_has_answer_origin)
+    private URI origin;
+
+    @OWLObjectProperty(iri = Vocabulary.s_p_has_object_value)
+    private URI codeValue;
+
+    public Answer() {
+    }
+
+    public Answer(String textValue, URI origin, URI codeValue) {
+        this.id = UUID.randomUUID().toString();
+        this.uri = URI.create(Vocabulary.s_c_answer + "/" + id);
+        this.textValue = textValue;
+        this.origin = origin;
+        this.codeValue = codeValue;
+    }
+
+    public Answer(URI uri, String id, String textValue, URI origin, URI codeValue) {
+        this.uri = uri;
+        this.id = id;
+        this.textValue = textValue;
+        this.origin = origin;
+        this.codeValue = codeValue;
+    }
+
+    public String getTextValue() {
+        return textValue;
+    }
+
+    public void setTextValue(String textValue) {
+        this.textValue = textValue;
+    }
+
+    public URI getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(URI origin) {
+        this.origin = origin;
+    }
+
+    public URI getCodeValue() {
+        return codeValue;
+    }
+
+    public void setCodeValue(URI codeValue) {
+        this.codeValue = codeValue;
+    }
+}
