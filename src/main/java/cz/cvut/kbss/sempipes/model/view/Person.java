47521700da98411e454be7110bc28a7fe50a7f44
Yan Doroshenko <yan1994doroshenko@gmail.com>
2017-02-13 20:46:55 +0100

    [new] Added login/password

--- src/main/java/cz/cvut/kbss/sempipes/model/view/Person.java
+++ src/main/java/cz/cvut/kbss/sempipes/model/view/Person.java
@@ -1,12 +1,51 @@
 package cz.cvut.kbss.sempipes.model.view;
 
 import cz.cvut.kbss.jopa.model.annotations.OWLClass;
+import cz.cvut.kbss.jopa.model.annotations.OWLDataProperty;
+import cz.cvut.kbss.jopa.model.annotations.ParticipationConstraints;
+import cz.cvut.kbss.jopa.model.annotations.Types;
 import cz.cvut.kbss.sempipes.model.AbstractEntity;
 import cz.cvut.kbss.sempipes.model.Vocabulary;
+
+import java.util.Set;
 
 /**
  * Created by Yan Doroshenko (yandoroshenko@protonmail.com) on 18.01.17.
  */
 @OWLClass(iri = Vocabulary.s_c_Person)
 public class Person extends AbstractEntity {
+
+    @ParticipationConstraints(nonEmpty = true)
+    @OWLDataProperty(iri = Vocabulary.s_p_has_username)
+    private String username;
+
+    @OWLDataProperty(iri = Vocabulary.s_p_has_password)
+    private String password;
+
+    @Types
+    private Set<String> types;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<String> getTypes() {
+        return types;
+    }
+
+    public void setTypes(Set<String> types) {
+        this.types = types;
+    }
 }
