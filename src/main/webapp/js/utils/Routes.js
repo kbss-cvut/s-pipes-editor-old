eb4ffdb1fc49231dcd386d8d11384b26b727a5cd
YanDoroshenko <yan1994doroshenko@gmail.com>
2017-06-18 13:31:32 +0200

    [upd] Cleanup

--- src/main/webapp/js/utils/Routes.js
+++ src/main/webapp/js/utils/Routes.js
@@ -6,9 +6,6 @@ var Routes = {
     users: {name: 'users', path: 'users'},
     createUser: {name: 'createUser', path: 'users/create'},
     editUser: {name: 'editUser', path: 'users/:username'},
-    clinics: {name: 'clinics', path: 'clinics'},
-    createClinic: {name: 'createClinic', path: 'clinics/create'},
-    editClinic: {name: 'editClinic', path: 'clinics/:key'},
     records: {name: 'records', path: 'records'},
     createRecord: {name: 'createRecord', path: 'records/create'},
     editRecord: {name: 'editRecord', path: 'records/:key'}
