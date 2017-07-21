6b4871f8168c5c2f2f0cbbe2c2f5a00208618356
YanDoroshenko <yan1994doroshenko@gmail.com>
2017-07-21 13:34:05 +0200

    [new] Handling errors from s-pipes request

--- src/main/scala/cz/cvut/kbss/spipes/rest/QAController.scala
+++ src/main/scala/cz/cvut/kbss/spipes/rest/QAController.scala
@@ -1,5 +1,6 @@
 package cz.cvut.kbss.spipes.rest
 
+import cz.cvut.kbss.spipes.exception.SpipesException
 import cz.cvut.kbss.spipes.service.QAService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.{HttpStatus, ResponseEntity}
@@ -18,14 +19,16 @@ class QAController {
   @PostMapping(path = Array("/{id}/form"), produces = Array("application/json"))
   def generateForm(@PathVariable id: String): ResponseEntity[Any] =
     service.generateForm(id) match {
-      case Some(response) => new ResponseEntity(response, HttpStatus.OK)
-      case None => new ResponseEntity("Node with id " + id + " not found", HttpStatus.NOT_FOUND)
+      case Right(response) => new ResponseEntity(response, HttpStatus.OK)
+      case Left(e: SpipesException) => new ResponseEntity(e.toString(), e.status)
+      case Left(e) => new ResponseEntity(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR)
     }
 
   @GetMapping(path = Array("/{id}/form"), produces = Array("application/json"))
   def gf(@PathVariable id: String): ResponseEntity[Any] =
     service.generateForm(id) match {
-      case Some(response) => new ResponseEntity(response, HttpStatus.OK)
-      case None => new ResponseEntity("Node with id " + id + " not found", HttpStatus.NOT_FOUND)
+      case Right(response) => new ResponseEntity(response, HttpStatus.OK)
+      case Left(e: SpipesException) => new ResponseEntity(e.toString(), e.status)
+      case Left(e) => new ResponseEntity(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR)
     }
 }
--- src/main/scala/cz/cvut/kbss/spipes/service/QAService.scala
+++ src/main/scala/cz/cvut/kbss/spipes/service/QAService.scala
@@ -2,14 +2,17 @@ package cz.cvut.kbss.spipes.service
 
 import java.net.URI
 
+import cz.cvut.kbss.spipes.exception.SpipesException
 import cz.cvut.kbss.spipes.model.Vocabulary
 import cz.cvut.kbss.spipes.model.view.Node
 import cz.cvut.kbss.spipes.persistence.dao.QADao
 import cz.cvut.kbss.spipes.rest.dto.RawJson
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.{HttpEntity, HttpHeaders, HttpMethod, HttpStatus}
+import org.springframework.http._
 import org.springframework.stereotype.Service
-import org.springframework.web.client.RestTemplate
+import org.springframework.web.client.{HttpClientErrorException, RestTemplate}
+
+import scala.util.{Failure, Success, Try}
 
 /**
   * Created by Yan Doroshenko (yandoroshenko@protonmail.com) on 22.12.16.
@@ -26,23 +29,30 @@ class QAService {
   def getNodeById(id: String): Option[Node] =
     dao.get(URI.create(Vocabulary.s_c_node + "/" + id))
 
-  def generateForm(uri: String): Option[RawJson] = {
-    val url = "https://kbss.felk.cvut.cz/sempipes-vfn-dev/service"
-    import org.springframework.web.util.UriComponentsBuilder
-    val builder = UriComponentsBuilder.fromHttpUrl(url)
-      .queryParam("_pId", "generate-fss-form")
-      .queryParam("formGenRepositoryUrl", "http://onto.fel.cvut.cz/rdf4j-server/repositories/fss-study-dev-formgen")
-      .queryParam("recordGraphId", "http://vfn.cz/ontologies/study-manager/formGen1496673137843")
-      .queryParam("repositoryUrl", "http://onto.fel.cvut.cz/rdf4j-server/repositories/fss-study-dev-app")
-    val r = restTemplate.exchange(builder.build().encode().toUri,
-      HttpMethod.GET,
-      new HttpEntity[AnyRef](null, new HttpHeaders()),
-      classOf[String])
-    r.getStatusCode() match {
-      case HttpStatus.OK =>
-        Some(RawJson(r.getBody()))
-      case _ =>
-        None
+  def generateForm(uri: String): Either[Throwable, RawJson] = {
+    Try {
+      val url = "https://kbss.felk.cvut.cz/sempipes-vfn-dev/service"
+      import org.springframework.web.util.UriComponentsBuilder
+      val builder = UriComponentsBuilder.fromHttpUrl(url)
+        .queryParam("_pId", "generate-fss-form")
+        .queryParam("formGenRepositoryUrl", "http://onto.fel.cvut.cz/rdf4j-server/repositories/fss-study-dev-formgen")
+        .queryParam("recordGraphId", "http://vfn.cz/ontologies/study-manager/formGen1496673137843")
+        .queryParam("repositoryUrl", "http://onto.fel.cvut.cz/rdf4j-server/repositories/fss-study-dev-app")
+      val r = restTemplate.exchange(builder.build().encode().toUri,
+        HttpMethod.GET,
+        new HttpEntity[AnyRef](null, new HttpHeaders()),
+        classOf[String])
+      r
+    }
+    match {
+      case Success(r: ResponseEntity[String]) if r.getStatusCode() == HttpStatus.OK =>
+        Right(RawJson(r.getBody()))
+      case Success(r: ResponseEntity[String]) =>
+        Left(new SpipesException(r.getStatusCode(), r.getBody()))
+      case Failure(e: HttpClientErrorException) =>
+        Left(new SpipesException(e.getStatusCode(), e.getLocalizedMessage()))
+      case Failure(e) =>
+        Left(e)
     }
   }
 }
