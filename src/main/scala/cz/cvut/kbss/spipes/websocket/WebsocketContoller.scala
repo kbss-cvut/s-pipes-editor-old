bab7a8d590e10ba91002418697660b216063f9ad
YanDoroshenko <yan1994doroshenko@gmail.com>
2017-07-22 17:49:12 +0200

    [upd] Better FS watcher implementation

--- src/main/scala/cz/cvut/kbss/spipes/websocket/WebsocketContoller.scala
+++ src/main/scala/cz/cvut/kbss/spipes/websocket/WebsocketContoller.scala
@@ -1,6 +1,6 @@
 package cz.cvut.kbss.spipes.websocket
 
-import java.nio.file.{Paths, StandardWatchEventKinds}
+import java.nio.file.{Paths, StandardWatchEventKinds, WatchKey}
 import javax.websocket.server.ServerEndpoint
 import javax.websocket.{OnClose, OnError, OnOpen, Session}
 
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.InitializingBean
 import org.springframework.stereotype.Controller
 
+import scala.annotation.tailrec
 import scala.collection.JavaConverters.asScalaBufferConverter
 import scala.collection.parallel.mutable
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -25,7 +26,7 @@ class WebsocketContoller extends InitializingBean {
 
   private val path = Paths.get("/home/yan/Audiobooks")
   private val ws = path.getFileSystem().newWatchService()
-  path.register(ws, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_DELETE, StandardWatchEventKinds.ENTRY_MODIFY)
+  private val wk = path.register(ws, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_DELETE, StandardWatchEventKinds.ENTRY_MODIFY)
 
   @OnOpen
   def onOpen(s: Session): Unit = {
@@ -46,25 +47,30 @@ class WebsocketContoller extends InitializingBean {
   }
 
   override def afterPropertiesSet(): Unit = {
-    Future {
-      while (true) { // fixme More reactive? At least not while true
-        val wk = ws.take()
+
+    @tailrec
+    def watchFS(wk: WatchKey): Unit =
+      if (wk.isValid()) {
         Try {
           val es = wk.pollEvents().asScala
           es.foreach(e => log.info("Registered file system event " + e.kind() + " on file " + path + "/" + e.context()))
-          synchronized(WebsocketContoller.sessions.foreach(s => {
+          WebsocketContoller.sessions.foreach(s => {
             log.info("Sending list of events to: " + s.toString())
             s.getBasicRemote.sendText(es.mkString("", "\n", "\n"))
-          }))
-          wk.reset()
+          })
         } match {
           case Failure(t) =>
             wk.reset()
             log.error(t.getLocalizedMessage())
             log.error(t.getStackTrace().mkString("\n"))
           case _ =>
+            wk.reset()
         }
+        watchFS(ws.take())
       }
+
+    Future {
+      watchFS(ws.take())
     }
   }
 }
