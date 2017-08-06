f3a7988df4c9d7e6b1ac090c8c031415f1c55b03
Yan Doroshenko <yan1994doroshenko@gmail.com>
2017-08-06 18:48:04 +0200

    [fix] Tests fixed

--- src/test/scala/cz/cvut/kbss/spipes/test/rest/ScriptControllerTest.scala
+++ src/test/scala/cz/cvut/kbss/spipes/test/rest/ScriptControllerTest.scala
@@ -2,12 +2,12 @@ package cz.cvut.kbss.spipes.test.rest
 
 import cz.cvut.kbss.spipes.model.spipes.Context
 import cz.cvut.kbss.spipes.service.SpipesService
-import cz.cvut.kbss.spipes.util.ConfigParam
 import cz.cvut.kbss.spipes.util.ConfigParam._
 import org.junit.Assert.{assertEquals, assertTrue}
 import org.junit.Test
 import org.mockito.Mockito
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.core.env.Environment
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
@@ -20,12 +20,23 @@ import scala.util.{Failure, Success}
 class ScriptControllerTest extends BaseControllerTestRunner {
 
   @Autowired
-  var service: SpipesService = _
+  private var env: Environment = _
+
+  @Autowired
+  private var service: SpipesService = _
+
+  @Test
+  def getScriptsReturnsNothing {
+    Mockito.when(service.getScripts(env.getProperty(SPIPES_LOCATION.value) + "/scripts")).thenReturn(Success(Set()))
+    val result = mockMvc.perform(get("/scripts")).andExpect(status.isOk).andReturn
+    val message = result.getResponse.getContentAsString
+    assertEquals("[]", message)
+  }
 
   @Test
   def getScriptsReturnsFailure {
-    Mockito.when(service.getScripts(ConfigParam.SPIPES_LOCATION + "/scripts")).thenReturn(Failure(new Throwable()))
-    val result = mockMvc.perform(get("/scripts")).andExpect(status.isNotFound).andReturn
+    Mockito.when(service.getScripts(env.getProperty(SPIPES_LOCATION.value) + "/scripts")).thenReturn(Failure(new Throwable()))
+    val result = mockMvc.perform(get("/scripts")).andExpect(status.isInternalServerError).andReturn
     val message = result.getResponse.getContentAsString
     assertTrue(message.isEmpty)
   }
@@ -33,7 +44,7 @@ class ScriptControllerTest extends BaseControllerTestRunner {
   @Test
   def getScriptReturnsNotFound {
     val id = "someRandomId"
-    Mockito.when(service.getScript(SPIPES_LOCATION + "/contexts", id)).thenReturn(Failure(new Throwable()))
+    Mockito.when(service.getScript(env.getProperty(SPIPES_LOCATION.value) + "/contexts", id)).thenReturn(Failure(new Throwable()))
     val result = mockMvc.perform(get("/script/" + id)).andExpect(status.isNotFound).andReturn
     val message = result.getResponse.getContentAsString
     assertTrue(message.isEmpty)
@@ -43,7 +54,7 @@ class ScriptControllerTest extends BaseControllerTestRunner {
   def getScriptReturnsScript {
     val context = new Context(null, null)
     val id = context.getId()
-    Mockito.when(service.getScript(SPIPES_LOCATION + "/contexts", id)).thenReturn(Success(Some(context)))
+    Mockito.when(service.getScript(env.getProperty(SPIPES_LOCATION.value) + "/contexts", id)).thenReturn(Success(Some(context)))
     val result = mockMvc.perform(get("/scripts/" + id)).andExpect(status.isOk).andReturn
     val message = result.getResponse.getContentAsString
     assertEquals("{\"uri\":\"" + context.getUri() + "\",\"id\":\"" + id + "\",\"label\":null,\"comment\":null}", message)
