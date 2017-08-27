fac65d57bccd52ed998496ef51ddd32cebbfee29
Yan Doroshenko <yan1994doroshenko@gmail.com>
2017-08-27 10:55:02 +0200

    [new] Actually useful tests

---
+++ src/test/scala/cz/cvut/kbss/spipes/test/service/SpipesServiceTest.scala
@@ -0,0 +1,93 @@
+package cz.cvut.kbss.spipes.test.service
+
+import java.io.FileNotFoundException
+import java.util
+
+import cz.cvut.kbss.spipes.model.spipes.{Module, ModuleType}
+import cz.cvut.kbss.spipes.persistence.dao.SpipesDao
+import cz.cvut.kbss.spipes.service.SpipesService
+import org.junit.Assert._
+import org.junit.Test
+import org.mockito.Mockito.when
+import org.springframework.beans.factory.annotation.Autowired
+
+import scala.collection.JavaConverters.asScalaBufferConverter
+import scala.util.{Failure, Random, Success}
+
+/**
+  * Created by Yan Doroshenko (yandoroshenko@protonmail.com) on 27.08.2017.
+  */
+class SpipesServiceTest extends BaseServiceTestRunner {
+
+  @Autowired
+  private var dao: SpipesDao = _
+
+  @Autowired
+  private var service: SpipesService = _
+
+  private val fileName = ""
+
+  @Test
+  def moduleTypesGotFailureFileNotFound: Unit = {
+    when(dao.getModuleTypes(fileName)).thenReturn(Failure(new FileNotFoundException()))
+    assertEquals(Right(None), service.getModuleTypes(fileName))
+  }
+
+  @Test
+  def moduleTypesGotFailureOther: Unit = {
+    val e = new IllegalArgumentException()
+    when(dao.getModuleTypes(fileName)).thenReturn(Failure(e))
+    assertEquals(Left(e), service.getModuleTypes(fileName))
+  }
+
+  @Test
+  def moduleTypesGotNullSuccess: Unit = {
+    when(dao.getModuleTypes(fileName)).thenReturn(Success(null))
+    assertEquals(Right(None), service.getModuleTypes(fileName))
+  }
+
+  @Test
+  def moduleTypesGotEmptySuccess: Unit = {
+    when(dao.getModuleTypes(fileName)).thenReturn(Success(new util.LinkedList[ModuleType]()))
+    assertEquals(Right(None), service.getModuleTypes(fileName))
+  }
+
+  @Test
+  def moduleTypesGotNonEmptySuccess: Unit = {
+    val l = new util.LinkedList[ModuleType]()
+    val size = Random.nextInt(100) + 1
+    Seq.fill(size)(0).foreach((_) =>
+      l.add(new ModuleType()))
+    when(dao.getModuleTypes(fileName)).thenReturn(Success(l))
+    assertEquals(Right(Some(l.asScala)), service.getModuleTypes(fileName))
+  }
+
+  @Test
+  def modulesGotFailureFileNotFound: Unit = {
+    when(dao.getModules(fileName)).thenReturn(Failure(new FileNotFoundException()))
+    assertEquals(Right(None), service.getModules(fileName))
+  }
+
+  @Test
+  def modulesGotFailureOther: Unit = {
+    val e = new IllegalArgumentException()
+    when(dao.getModules(fileName)).thenReturn(Failure(e))
+    assertEquals(Left(e), service.getModules(fileName))
+  }
+
+  @Test
+  def modulesGotEmptySuccess: Unit = {
+    when(dao.getModules(fileName)).thenReturn(Success(new util.LinkedList[Module]()))
+    assertEquals(Right(None), service.getModules(fileName))
+  }
+
+  @Test
+  def modulesGotNonEmptySuccess: Unit = {
+    val l = new util.LinkedList[Module]()
+    val size = Random.nextInt(100) + 1
+    Seq.fill(size)(0).foreach((_) =>
+      l.add(new Module()))
+    when(dao.getModules(fileName)).thenReturn(Success(l))
+    assertEquals(Right(Some(l.asScala)), service.getModules(fileName))
+  }
+}
---
+++ src/test/scala/cz/cvut/kbss/spipes/test/service/ViewServiceTest.scala
@@ -0,0 +1,79 @@
+package cz.cvut.kbss.spipes.test.service
+
+import java.io.FileNotFoundException
+import java.util
+
+import cz.cvut.kbss.spipes.model.spipes.Module
+import cz.cvut.kbss.spipes.model.view.{Edge, Node, View}
+import cz.cvut.kbss.spipes.persistence.dao.{SpipesDao, ViewDao}
+import cz.cvut.kbss.spipes.service.ViewService
+import org.junit.Assert._
+import org.junit.Test
+import org.mockito.Mockito.when
+import org.springframework.beans.factory.annotation.Autowired
+
+import scala.collection.JavaConverters.{asScalaBufferConverter, setAsJavaSetConverter}
+import scala.util.{Failure, Random, Success}
+
+/**
+  * Created by Yan Doroshenko (yandoroshenko@protonmail.com) on 27.08.2017.
+  */
+class ViewServiceTest extends BaseServiceTestRunner {
+
+  @Autowired
+  private var viewDao: ViewDao = _
+
+  @Autowired
+  private var spipesDao: SpipesDao = _
+
+  @Autowired
+  private var service: ViewService = _
+
+  private val fileName = ""
+
+  // Fixme: The following tests depend on correct implementation of SpipesService
+  // Fixme: due to mysterious Spring processes
+  // Fixme: that don't allow for proper SpipesService mock autowiring
+
+  @Test
+  def spipesServiceGotFailureFileNotFound: Unit = {
+    val e = new FileNotFoundException()
+    when(spipesDao.getModules(fileName)).thenReturn(Failure(e))
+    assertEquals(Right(None), service.newViewFromSpipes(fileName))
+  }
+
+  @Test
+  def spipesServiceGotFailureOther: Unit = {
+    val e = new IllegalArgumentException()
+    when(spipesDao.getModules(fileName)).thenReturn(Failure(e))
+    assertEquals(Left(e), service.newViewFromSpipes(fileName))
+  }
+
+  @Test
+  def spipesServiceGotNullSuccess: Unit = {
+    when(spipesDao.getModules(fileName)).thenReturn(Success(null))
+    assertEquals(Right(None), service.newViewFromSpipes(fileName))
+  }
+
+  @Test
+  def spipesServiceGotEmptySuccess: Unit = {
+    when(spipesDao.getModules(fileName)).thenReturn(Success(new util.LinkedList[Module]()))
+    assertEquals(Right(None), service.newViewFromSpipes(fileName))
+  }
+
+  @Test
+  def spipesServiceGotNonEmptySuccess: Unit = {
+    val l = new util.LinkedList[Module]()
+    val size = Random.nextInt(100) + 1
+    Seq.fill(size)(0).foreach((_) =>
+      l.add(new Module()))
+    val v = new View(fileName, l.asScala.map((m) => {
+      val n = new Node()
+      n.setInParameters(new util.HashSet[String]())
+      n.setOutParameters(new util.HashSet[String]())
+      n
+    }).toSet.asJava, new util.HashSet[Edge]())
+    when(spipesDao.getModules(fileName)).thenReturn(Success(l))
+    assertEquals(Right(Some(v)), service.newViewFromSpipes(fileName))
+  }
+}
