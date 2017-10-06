cf9296443d8c96fcc7bfb9f161738dc7fb719a25
Yan Doroshenko <yan1994doroshenko@gmail.com>
2017-10-07 01:08:16 +0200

    [upd] Refactoring

--- src/main/webapp/js/utils/Utils.js
+++ src/main/webapp/js/utils/Utils.js
@@ -2,18 +2,6 @@
 
 const Constants = require('../constants/Constants');
 
-/**
- * Common propositions that should not be capitalized
- */
-const PREPOSITIONS = [
-    'a', 'about', 'across', 'after', 'along', 'among', 'an', 'around', 'as', 'aside', 'at', 'before', 'behind', 'below',
-    'beneath', 'beside', 'besides', 'between', 'beyond', 'but', 'by', 'for', 'given', 'in', 'inside', 'into', 'like', 'near',
-    'of', 'off', 'on', 'onto', 'outside', 'over', 'since', 'than', 'through', 'to', 'until', 'up', 'via', 'with', 'within',
-    'without', 'not'
-];
-
-const URL_CONTAINS_QUERY = /^.+\?.+=.+$/;
-
 const CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
 module.exports = {
@@ -57,66 +45,6 @@ module.exports = {
     },
 
     /**
-     * Returns a Java constant (uppercase with underscores) as a nicer string.
-     *
-     * Replaces underscores with spaces. And if capitalize is selected, capitalizes the words.
-     */
-    constantToString: function (constant, capitalize) {
-        if (!capitalize) {
-            return constant.replace(/_/g, ' ');
-        }
-        const words = constant.split('_');
-        for (let i = 0, len = words.length; i < len; i++) {
-            const word = words[i];
-            if (i > 0 && PREPOSITIONS.indexOf(word.toLowerCase()) !== -1) {
-                words[i] = word.toLowerCase();
-            } else {
-                words[i] = word.charAt(0) + word.substring(1).toLowerCase();
-            }
-        }
-        return words.join(' ');
-    },
-
-    /**
-     * Converts the specified time value from one time unit to the other.
-     *
-     * Currently supported units are seconds, minutes and hours. When converting to larger units (e.g. from seconds to
-     * minutes), the result is rounded to integer.
-     *
-     * @param fromUnit Unit to convert from
-     * @param toUnit Target unit
-     * @param value The value to convert
-     * @return {*} Converted value
-     */
-    convertTime: function (fromUnit, toUnit, value) {
-        if (fromUnit === toUnit) {
-            return value;
-        }
-        switch (fromUnit) {
-            case 'second':
-                if (toUnit === 'minute') {
-                    return Math.round(value / 60);
-                } else {
-                    return Math.round(value / 60 / 60);
-                }
-            case 'minute':
-                if (toUnit === 'second') {
-                    return 60 * value;
-                } else {
-                    return Math.round(value / 60);
-                }
-            case 'hour':
-                if (toUnit === 'second') {
-                    return 60 * 60 * value;
-                } else {
-                    return 60 * value;
-                }
-            default:
-                return value;
-        }
-    },
-
-    /**
      * Extracts report key from location header in the specified Ajax response.
      * @param response Ajax response
      * @return {string} Report key as string
@@ -139,88 +67,6 @@ module.exports = {
         const hash = window.location.hash;
         const result = /#[/]?([a-z/0-9]+)\?/.exec(hash);
         return result ? result[1] : '';
-    },
-
-    /**
-     * Generates a random integer value between 0 and 2^30 (approx. max Java integer / 2).
-     *
-     * The reason the number is Java max integer / 2 is to accommodate possible increments of the result.
-     * @return {number}
-     */
-    randomInt: function () {
-        const min = 0,
-            max = 1073741824;   // Max Java Integer / 2
-        return Math.floor(Math.random() * (max - min)) + min;
-    },
-
-    /**
-     * Maps the specified id to a name based on a matching item.
-     *
-     * This function assumes that the items have been processed by {@link #jsonLdToTypeaheadOption), so the id should
-     * be equal to one of the item's 'id' attribute, and if it is, the item's 'name' is returned.
-     * @param items The items containing also mapping for the specified value (presumably)
-     * @param id The id to map, probably a URI
-     * @return {*}
-     */
-    idToName: function (items, id) {
-        if (!items) {
-            return id;
-        }
-        for (let i = 0, len = items.length; i < len; i++) {
-            if (items[i].id === id) {
-                return items[i].name;
-            }
-        }
-        return id;
-    },
-
-    /**
-     * Gets the last path fragment from the specified URL.
-     *
-     * I.e. it returns the portion after the last '/'
-     * @param url
-     * @return {string|*}
-     */
-    getLastPathFragment: function (url) {
-        return url.substring(url.lastIndexOf('/') + 1);
-    },
-
-    /**
-     * Calculates a simple hash of the specified string, much like usual Java implementations.
-     * @param str The string to compute has for
-     * @return {number}
-     */
-    getStringHash: function (str) {
-        let hash = 0,
-            strlen = str ? str.length : 0,
-            c;
-        if (strlen === 0) {
-            return hash;
-        }
-        for (let i = 0; i < strlen; i++) {
-            c = str.charCodeAt(i);
-            hash = ((hash << 5) - hash) + c;
-            hash = hash & hash; // Convert to 32bit integer
-        }
-        return hash;
-    },
-
-    /**
-     * Appends parameters in the specified argument as query parameters to the specified url.
-     *
-     * The url can already contain a query string
-     * @param url The URL to append parameters to
-     * @param parameters The parameters to add
-     * @return {*} Updated URL
-     */
-    addParametersToUrl: function (url, parameters) {
-        if (parameters) {
-            url += URL_CONTAINS_QUERY.test(url) ? '&' : '?';
-            Object.getOwnPropertyNames(parameters).forEach(function (param) {
-                url += param + '=' + parameters[param] + '&';   // '&' at the end of request URI should not be a problem
-            });
-        }
-        return url;
     },
 
     generatePassword: function () {
