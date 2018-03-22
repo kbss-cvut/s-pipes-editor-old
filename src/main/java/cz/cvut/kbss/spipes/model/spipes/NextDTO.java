9608677a083d87d328743c43256e380215443a7b
Yan Doroshenko <yan1994doroshenko@gmail.com>
2018-03-22 09:03:25 +0100

    [fix] Fix absent identifier for NextDTO

--- src/main/java/cz/cvut/kbss/spipes/model/spipes/NextDTO.java
+++ src/main/java/cz/cvut/kbss/spipes/model/spipes/NextDTO.java
@@ -2,6 +2,7 @@ package cz.cvut.kbss.spipes.model.spipes;
 
 import cz.cvut.kbss.jopa.model.annotations.OWLClass;
 import cz.cvut.kbss.jopa.model.annotations.OWLDataProperty;
+import cz.cvut.kbss.spipes.model.AbstractEntity;
 
 import static cz.cvut.kbss.spipes.model.Vocabulary.s_c_next_dto;
 import static cz.cvut.kbss.spipes.model.Vocabulary.s_p_has_source_uri;
@@ -10,7 +11,7 @@ import static cz.cvut.kbss.spipes.model.Vocabulary.s_p_has_source_uri;
  * Created by Yan Doroshenko (yandoroshenko@protonmail.com) on 22.03.2018.
  */
 @OWLClass(iri = s_c_next_dto)
-public class NextDTO {
+public class NextDTO extends AbstractEntity {
 
     @OWLDataProperty(iri = s_p_has_source_uri)
     private String sourceUri;
