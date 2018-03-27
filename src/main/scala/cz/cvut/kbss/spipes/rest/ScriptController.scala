b5de640d3d6c32eb8ffe84d73810aa28fd277178
Yan Doroshenko <yan1994doroshenko@gmail.com>
2018-03-27 17:36:19 +0200

    [fix] Fix imports

--- src/main/scala/cz/cvut/kbss/spipes/rest/ScriptController.scala
+++ src/main/scala/cz/cvut/kbss/spipes/rest/ScriptController.scala
@@ -3,6 +3,7 @@ package cz.cvut.kbss.spipes.rest
 import cz.cvut.kbss.jsonld.JsonLd
 import cz.cvut.kbss.spipes.model.spipes.{NextDTO, QuestionDTO}
 import cz.cvut.kbss.spipes.service.ScriptService
+import cz.cvut.kbss.spipes.util.Implicits._
 import cz.cvut.kbss.spipes.util.Logger
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.{HttpStatus, ResponseEntity}
