80b65b45fed38f29d61d9026e2c09708d17494d4
Yan Doroshenko <yan1994doroshenko@gmail.com>
2018-04-16 21:58:32 +0200

    [new] Crash if script location not found

--- src/main/scala/cz/cvut/kbss/spipes/util/ScriptManager.scala
+++ src/main/scala/cz/cvut/kbss/spipes/util/ScriptManager.scala
@@ -1,6 +1,6 @@
 package cz.cvut.kbss.spipes.util
 
-import java.io.File
+import java.io.{File, FileNotFoundException}
 
 import cz.cvut.kbss.spipes.util.ConfigParam.SCRIPTS_LOCATION
 import cz.cvut.kbss.spipes.util.Implicits._
@@ -11,7 +11,13 @@ import scala.io.Source
   * Created by Yan Doroshenko (yandoroshenko@protonmail.com) on 14.04.2018.
   */
 trait ScriptManager extends PropertySource {
-  protected def discoverLocations: Array[File] = getProperty(SCRIPTS_LOCATION).split(";").map(new File(_))
+  protected def discoverLocations: Array[File] = getProperty(SCRIPTS_LOCATION).split(";").map(s => {
+    val f = new File(s)
+    if (!f.exists())
+      throw new FileNotFoundException(f"""Scripts location not found: $f""")
+    else
+      f
+  })
 
   protected def ignored: Set[File] =
     discoverLocations.flatMap(f => {
